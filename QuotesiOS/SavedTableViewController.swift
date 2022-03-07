df5a8b40c4ddb4eab9cc61a2b752c30d42ed65f8
Kunioshi <filipekunioshi@hotmail.com>
2022-03-07 20:01:25 -0300

    Implement Delete Quote from SavedTableVC

--- QuotesiOS/SavedTableViewController.swift
+++ QuotesiOS/SavedTableViewController.swift
@@ -44,17 +44,17 @@ class SavedTableViewController: UITableViewController {
     }
     */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
+            let delQuote = QuoteController.quoteList[indexPath.row]
+            if QuoteController.deleteQuote(id: delQuote.id!) {
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
     /*
     // Override to support rearranging the table view.
